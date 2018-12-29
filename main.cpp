dceb5ba61df61233180bfe6d1bfa259fe82292bc
H0lm3s <samuele.serrago@aol.com>
2018-12-30 00:05:00 +0100

    Fixed code and fixed unlimited number of cameras

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
-#include "streamer.h"
-#include <string>
 #include <iostream>
+#include "streamer.h"
 
 
 int main(int argc, char *argv[])
 {
-    int port;
-    int address;
+    int port, cam_id;
     if (argc > 2) {
         port = std::atoi(argv[1]);
-        address = std::atoi(argv[2]);
-        std::cout << "Wrapping camera " << address << " su porta: " << port << std::endl;
-        Streamer s(port, address);
+        cam_id = std::atoi(argv[2]);
+        std::cout << "Binding camera on port: " << port << std::endl;
+        Streamer s(3852, "http://192.168.1.67:3851");
         s.start();
+
+        std::string str;
+        std::cout << "Write something to exit" << std::endl;
+        std::cin >> str;
+
+        s.stop();
+
     } else
-        std::cout << "Argomenti mancanti. Inserisci porta e numero porta usb" << std::endl;
+        std::cout << "Missed arguments. Insert port and number of camera" << std::endl;
     return 0;
 }
--- streamer.h
+++ streamer.h
@@ -1,36 +1,158 @@
-﻿#ifndef STREAMER_H
+#ifndef STREAMER_H
 #define STREAMER_H
+
+#include <opencv2/highgui/highgui.hpp>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 #include <iostream>
-#include "camera.h"
-#include <thread>
-#include <array>
-#include <atomic>
+#include <mutex>
+#include <future>
 
 
-class Streamer {  
+//---------------------------------- Print error -----------------------------------------
+
+void error [[noreturn]] (std::string msg) {
+    perror(msg.c_str());
+    exit(1);
+}
+
+/**
+ * @brief HEADER
+ * HTTP response header
+ */
+static const std::string HEADER = "HTTP/1.1 200 OK\r\n"
+                                  "Connection: close\r\n"
+                                  "Max-Age: 0\r\n"
+                                  "Expires: 0\r\n"
+                                  "Cache-Control: no-cache, private\r\n"
+                                  "Pragma: no-cache\r\n"
+                                  "Content-Type: multipart/x-mixed-replace;boundary=--boundary\r\n\r\n";
+
+/**
+ * @brief HEADER
+ * HTTP response BODY. To be completed before sending
+ */
+static const std::string BODY = "--boundary\r\n"
+                                "Content-Type: image/jpeg\r\n"
+                                "Content-Length: ";
+
+/**
+ * @brief init_server
+ * @param port
+ * @return a socketaddr_in struct that contain server information
+ */
+sockaddr_in init_server(uint16_t port) {
+    sockaddr_in s;
+    bzero(reinterpret_cast<char*>(&s), sizeof(s));
+    s.sin_family = PF_INET;
+    s.sin_addr.s_addr = inet_addr("127.0.0.1");
+    s.sin_port = htons(port);
+    return s;
+}
+
+//---------------------------------- Camera handle -----------------------------------------
+
+/**
+ * @brief The Camera handler class
+ * Handle an OpenCV VideoCapture object in order to capture frames and convert them in jpg
+ */
+class Camera {
 private:
-    static const int MAX_CONNECTIONS = 16;
-    int server, client;
-    struct sockaddr_in serverAddress, clientAddress;
-    Camera* camera;
-    std::thread* threadList[MAX_CONNECTIONS];
-    std::array<std::atomic<bool>, MAX_CONNECTIONS> threadTerminated;
-
-    void error(std::string msg);
-    void bindChannel();
-    void stream(int socket, int index);
-    int getFirstFreePos();
+    std::mutex l;
+    cv::VideoCapture c;
+    cv::Mat get_frame() { cv::Mat m; l.lock(); c >> m; l.unlock(); return m; }
 
 public:
-    void start();
-    void stop();
-
-    Streamer(int port = 3851, int cameraAddress = 0);
-    Streamer(int port, std::string cameraAddress);
-    ~Streamer();
+    Camera(int n) : c(n) {}
+    Camera(std::string n) : c(n) {}
+    ~Camera() { c.release(); }
+    std::string jpg_encode() {
+        std::vector<uchar> buf;
+        cv::imencode(".jpg", get_frame(), buf);
+        return std::string(buf.begin(), buf.end());
+    }
 };
+
+//---------------------------------- Some functions ----------------------------------------
+
+/**
+ * @brief is_ready
+ * @param f
+ * @return true if the std::future has complete its compute, false otherwise
+ */
+template<typename T>
+bool is_ready(std::future<T> const& f) { return f.wait_for(std::chrono::seconds(0)) == std::future_status::ready; }
+
+static const int nullpos = -1;
+
+/**
+ * @brief first_free
+ * @param f
+ * @return the position of the first free std::future in a container
+ */
+template<typename V>
+int first_free(V& f) {
+    for (size_t i = 0; i < f.size(); ++i)
+        if (is_ready(f[i])) {
+            f[i].get();
+            return int(i);
+        }
+    return nullpos;
+}
+
+//---------------------------------- Streamer class -----------------------------------------
+
+/**
+ * @brief The Streamer class
+ * Streamer is the "server". It receive HTTP requests and send responses
+ */
+class Streamer {
+private:
+    Camera cam;
+    int server, client;
+    sockaddr_in s_addr, c_addr;
+    std::thread t;
+    void bind_channel() { if (bind(server, reinterpret_cast<sockaddr*>(&s_addr), sizeof(s_addr)) < 0) error("binding_failed"); }
+    void send_datas(int socket);
+    void run();
+
+public:
+    void start(){ bind_channel(); listen(server, 5); t = std::thread(&Streamer::run, this); }
+    void stop() { shutdown(server, SHUT_RDWR); t.join(); }
+    Streamer(uint16_t port, int cam_addr) : cam(cam_addr), server(socket(PF_INET, SOCK_STREAM, 0)), s_addr(init_server(port)) {}
+    Streamer(uint16_t port, std::string cam_addr) : cam(cam_addr), server(socket(PF_INET, SOCK_STREAM, 0)), s_addr(init_server(port)) {}
+
+};
+
+void Streamer::send_datas(int socket) {
+    std::string image, buffer = HEADER;
+    while ((send(socket, buffer.c_str(), buffer.size(), MSG_NOSIGNAL)) > 0) {
+        image = cam.jpg_encode();
+        buffer = BODY + std::to_string(image.size()) + "\r\n\r\n" + image;
+    }
+    close(socket);
+}
+
+void Streamer::run() {
+    std::vector<std::future<void>> tasks;
+    while(true) {
+        socklen_t c_length = sizeof(c_addr);
+        if ((client = accept(server, reinterpret_cast<sockaddr*>(&c_addr), &c_length)) > 0) {
+            if (int pos = first_free(tasks) == nullpos)
+                tasks.push_back(std::async(std::launch::async, &Streamer::send_datas, this, client));
+            else
+                tasks[static_cast<size_t>(pos)] = std::async(std::launch::async, &Streamer::send_datas, this, client);
+        } else {
+            for (auto& e : tasks)
+                e.get();
+            break;
+        }
+    }
+    close(server);
+}
 
 #endif // STREAMER_H
